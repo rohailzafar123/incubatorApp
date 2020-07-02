c63a6841e627e2b7dfe709568a3c99e726ace4a3
rohailzafar123 <rohaildvs30@gmail.com>
2020-07-02 20:36:37 +0500

    try font style and complete slider

--- scr/component/leftSlideBar/index.js
+++ scr/component/leftSlideBar/index.js
@@ -4,18 +4,30 @@ import style from './styles';
 import Modal from 'react-native-modal';
 import Menu from 'react-native-vector-icons/MaterialCommunityIcons';
 import Setting from 'react-native-vector-icons/Feather';
+import Lock from 'react-native-vector-icons/SimpleLineIcons';
+import SysSettings from 'react-native-vector-icons/Octicons';
+import User from 'react-native-vector-icons/EvilIcons';
+import Graph from 'react-native-vector-icons/Entypo';
+import Pass from 'react-native-vector-icons/MaterialCommunityIcons';
+import Data from 'react-native-vector-icons/FontAwesome';
+// import SysSettings from 'react-native-vector-icons/Octicons';
+
 
 const { height, width } = Dimensions.get('window');
 export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            isModalVisible:false,
+            LockorUnlo:false,
         }
 
     }
     toggleModal = () => {
         this.setState({ isModalVisible: !this.state.isModalVisible });
+    };
+    toggleModalLock = () => {
+        this.setState({ LockorUnlo: !this.state.LockorUnlo });
     };
     render() {
         return (
@@ -36,9 +48,9 @@ export default class App extends Component {
                         maxHeight: height * 1,
                         maxWidth: width * .35,
                         // top:width * .13,
-                        left: width * .58,
-                        // borderTopRightRadius:width * .05,
-                        // borderTopLeftRadius:width * .05,
+                        left: width * .6,
+                        borderTopLeftRadius:width * .02,
+                        borderBottomLeftRadius:width * .02,
                         // maxHeight:height * .7,
 
                     }}>
@@ -55,13 +67,16 @@ export default class App extends Component {
                                 justifyContent: "space-between",
                                 alignItems: 'center',
                                 flexDirection: 'row',
-                                paddingHorizontal: width * .02,
+                                paddingHorizontal: width * .015,
+                        borderTopLeftRadius:width * .02,
+
                             }}>
 
                                 <Text style={{
                                     fontSize: width * .02,
                                     color: 'red',
-                                    fontWeight: 'bold'
+                                    fontWeight: 'bold',
+                                    fontFamily:'MetalMania-Regular',
                                 }}>
                                     Settings
                                 </Text>
@@ -71,70 +86,67 @@ export default class App extends Component {
                         <View style={{
                             height: height * .73,
                             width: width * .35,
-                            backgroundColor:'red',
+                            // backgroundColor:'red',
                         }} >
                             <View style={{
                                 flex: 1,
                                 elevation: width * .005,
                                 justifyContent:'center',
                                 alignItems:'center',
+                                marginTop:height * .02,
+                                
                             }}>
-                                <View style={{
-                                    height:height * .09,
-                                    width: width * .3,
-                                    backgroundColor:'blue',
-                                    marginBottom:height * .01
-                                }}>
-
-                                </View>
-                                <View style={{
-                                    height:height * .09,
-                                    width: width * .3,
-                                    backgroundColor:'blue',
-                                    marginBottom:height * .01
-
-                                }}>
-
-                                </View>
-                                <View style={{
-                                    height:height * .09,
-                                    width: width * .3,
-                                    backgroundColor:'blue',
-                                    marginBottom:height * .01
-                                    
-                                }}>
-
-                                </View>
-                                <View style={{
-                                    height:height * .09,
-                                    width: width * .3,
-                                    backgroundColor:'blue',
-                                    marginBottom:height * .01
-
-                                }}>
-
-                                </View>
-                                <View style={{
-                                    height:height * .09,
-                                    width: width * .3,
-                                    backgroundColor:'blue',
-                                    marginBottom:height * .01
-
-                                }}>
-
-                                </View>
-                                <View style={{
-                                    height:height * .09,
-                                    width: width * .3,
-                                    backgroundColor:'blue',
-                                    marginBottom:height * .01
-
-                                }}>
-
-                                </View>
+                                <TouchableOpacity style={style.listView} onPress={this.toggleModalLock}>
+                                    <Text style={style.listText}>Lock Or Unlock</Text>
+                                    <Lock name={'lock'} size={width * .02} />
+                                </TouchableOpacity>
+                                <TouchableOpacity style={style.listView}>
+                                    <Text style={style.listText}>System Setting</Text>
+                                    <SysSettings name={'settings'} size={width * .02} />
+                                </TouchableOpacity>
+                                <TouchableOpacity style={style.listView}>
+                                    <Text style={style.listText}>Parameters</Text>
+                                    <User name={'user'} size={width * .025} />
+                                </TouchableOpacity>
+                                <TouchableOpacity style={style.listView}>
+                                    <Text style={style.listText}>Graph Setting</Text>
+                                    <Graph name={'area-graph'} size={width * .02} />
+                                </TouchableOpacity>
+                                <TouchableOpacity style={style.listView}>
+                                    <Text style={style.listText}>Calibaration</Text>
+                                    <Pass name={'lastpass'} size={width * .02} />
+                                </TouchableOpacity>
+                                <TouchableOpacity style={style.listView}>
+                                    <Text style={style.listText}>Data Record</Text>
+                                    <Data name={'database'} size={width * .018} />
+                                </TouchableOpacity>
+                                <TouchableOpacity style={style.listView}>
+                                    <Text style={style.listText}>Unit</Text>
+                                    <Text style={style.listText}>(c or f) (kg or lb)</Text>
+                                </TouchableOpacity>
                             </View>
 
                         </View>
+                    <Modal animationIn="slideInRight"
+                    animationOut="slideOutRight"
+                    onBackdropPress={() => this.toggleModalLock()}
+                    onSwipeComplete={() => this.toggleModalLock()}
+                    swipeDirection="right"
+                    isVisible={this.state.LockorUnlo}
+                    style={{
+                        elevation: width * .005,
+                        backgroundColor: 'white',
+                        maxHeight: height * 1,
+                        maxWidth: width * .35,
+                        // top:width * .13,
+                        // left: width * .6,
+                        // borderTopRightRadius:width * .05,
+                        // borderTopLeftRadius:width * .05,
+                        // maxHeight:height * .7,
+
+                    }}>
+
+                    </Modal>
                     </View>
                 </Modal>
             </View>
---
+++ scr/component/leftSlideBar/styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Dimensions } from "react-native"
+
+//Packages
+
+//Files
+const { height, width } = Dimensions.get('window');
+
+export default styles = StyleSheet.create({
+    listView:{
+        height:height * .08,
+        width: width * .3,
+        backgroundColor:'white',
+        marginBottom:height * .02,
+        elevation:width * .002,
+        borderWidth:width * .001,
+        borderColor:'#b098b4c5',
+        alignItems:"center",
+        justifyContent:'space-between',
+        flexDirection:'row',
+        paddingHorizontal:width * .01
+    },
+    listText:{
+        fontSize:width * .015,
+        fontWeight:'bold',
+        color:"#484149c5",
+        fontFamily:'MetalMania-Regular'
+    },
+});
